2b3e1b2d7223cbffaa980f3b7e1971f008a14fd3
Dave Anderson <dave@fastforwardtime.co.uk>
2023-03-16 11:13:48 +0000

    configure user and setup initial views
    Configure devise with user model and add name attribute, style application layout and flashes with tailwind templates, generate devise new session view and style with tailwind, configure authenticated and non-authenticated root paths

--- spec/system/homepages_spec.rb
+++ spec/system/homepages_spec.rb
@@ -3,8 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Homepages', type: :system do
-  it 'displays the homepage' do
+  let(:user) { create :user }
+
+  it 'displays the homepage when logged in' do
     visit root_path
+
+    expect(page).not_to have_content('Rails version')
+
+    click_on('Log in')
+    expect(page).to have_content('Invalid Email or password.')
+
+    fill_in('Email', with: user.email)
+    fill_in('Password', with: user.password)
+    click_on('Log in')
     expect(page).to have_content('Rails version')
   end
 end
